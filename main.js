604cf4672711e97e27d7c3265cb0f508e5c5055f
Hassane31 <hassane.laabane@gmail.com>
2025-07-12 23:55:50 +0100

    all in one

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
 let name = "hassane ";
 let age = 21 ;
-
 console.log(`my name is ${name} and my age is ${age }`)     
-
-
 //vd 8 ---------------------------------------------------------
-
 let num1 = 5 
 let num2 = 4
-
 console.log(num1 ** num2) //pow 
 console.log (5/4)
 num1 += 1 
 console.log(num1)
-
-
 //vd 10 --------------------------------------------------------
- 
 console.log('4'-'3')
 console.log(typeof Nan)
 console.log(-'5')
@@ -25,3 +17,20 @@ console.log(+true)
 console.log(+false)
 console.log(+null)
 //vd 11 ---------------------------------------------------------
+console.log(parseFloat('4.5'))
+console.log(Number(4) )
+console.log(parseInt('4 hassabe'))
+//vd 12 ---------------------------------------------------------
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.isSafeInteger(-9007199254740991))
+console.log(Number.isInteger(44))
+console.log(Number.isNaN('ahmad'-5))
+//vd 13 ---------------------------------------------------------
+console.log(Math.E)
+console.log(Math.PI)
+console.log(Math.abs(-5))
+console.log(Math.sqrt(4))
+console.log(Math.round(10.2))
+console.log(Math.max(1,2,4,5,6,8,9,0))
+//vd 14 ---------------------------------------------------------
