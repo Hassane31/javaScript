000916674b2fd415fcf2300e27f228da83a074c8
Hassane31 <hassane.laabane@gmail.com>
2025-07-13 00:28:20 +0100

    finish part 16

--- main.js
+++ main.js
@@ -34,3 +34,24 @@ console.log(Math.sqrt(4))
 console.log(Math.round(10.2))
 console.log(Math.max(1,2,4,5,6,8,9,0))
 //vd 14 ---------------------------------------------------------
+let X ;
+// X = window.prompt() 
+console.log(0.025 * X+'DA')
+//vd 15 ---------------------------------------------------------
+let Y =100 
+console.log(typeof Y)
+console.log(typeof String(Y))
+console.log(typeof Y.toString())
+console.log(typeof 100..toString())
+//vd 16 --------------------------------------------------------------
+console.log(name.repeat(4))
+console.log(name.length)
+
+console.log(name[0])//console.log(name[10]) -> undefinded
+console.log(name.charAt(0))//console.log(name.charAt(10)) -> '  '
+
+console.log(name.indexOf('n',4)) //start serch
+console.log(name.lastIndexOf('n'))
+console.log(name.slice(2,5))
+console.log(name.split('',3))
+//vd 17 --------------------------------------------------------------
