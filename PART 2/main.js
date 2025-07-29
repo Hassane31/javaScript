a94eaf4e71167abc6696ae97272be0671a595a25
Hassane31 <hassane.laabane@gmail.com>
2025-07-29 16:42:34 +0100

    add inner and outer ... sibling...

--- PART 2/main.js
+++ PART 2/main.js
@@ -31,4 +31,42 @@ links.href ='https://www.instagram.com'//change link
 
 let forms = document.forms
 console.log(forms)
-//////////////////// vd 49 //////////////////////////////
+// vd 49---<element attribute > text <element>--------------
+
+let img = document.getElementById('img')
+console.log(img.src)
+img.src = 'IMG2.png'
+console.log(img.hasAttributes())
+console.log(img.attributes)
+img.setAttribute('title', 'ahmed')
+console.log(img.title)
+img.setAttribute('src','IMG1.png')
+img.removeAttribute('alt')//remove
+
+// vd 50------inner and outer-------------------------------
+let container = document.getElementById('container')
+console.log(container)
+console.log(container.outerHTML)
+console.log(container.innerHTML)
+container.innerHTML = '<h1>hello JS </h1>'
+//container.outerHTML  = '<h1>hello JS </h1>'//delete <div>....
+
+//container.outerText = '<h1>hello JS </h1>'//delete <div>....
+container.innerText = '<h1>hello JS </h1>'
+
+// vd 51------sibling and parent-----------------------------
+let element = document.getElementById('second')
+
+console.log(element.previousElementSibling)
+console.log(element.nextElementSibling)
+element.nextElementSibling.innerHTML = 'text'
+
+element.previousElementSibling.remove()
+
+console.log(element.previousSibling)//return any thing previous
+console.log(element.previousElementSibling)//return jast elments
+
+//console.log(element.parentElement)
+//element.parentElement.remove()
+
+// vd 52------dom css style-------------------------------
